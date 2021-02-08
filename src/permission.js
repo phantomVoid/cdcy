71d23eef4d9bc1766eef693fe8de29bc6cc5a2c3
phantom <phantomsaber@foxmail.com>
2021-02-08 20:18:41 +0800

    [revise]permission添加mobile入口路由处理

--- src/permission.js
+++ src/permission.js
@@ -1,31 +1,48 @@
 import router from './router'
 import store from './store'
-import { Message } from 'element-ui'
+import {Message} from 'element-ui'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
-import { getToken } from '@/utils/auth'
+import {getToken} from '@/utils/auth'
 
-NProgress.configure({ showSpinner: false })
+NProgress.configure({showSpinner: false})
 
-const whiteList = ['/login', '/auth-redirect', '/index', '/register' ,'/mobile/login','/mobile/register']
+const whiteList = ['/index', '/login', '/auth-redirect', '/register', '/mobile/login', '/mobile/register','/mobile/index']
 
 router.beforeEach((to, from, next) => {
+  console.log("\n\n\n this is permission.js beforeEach function: >>> ")
+  console.log("from.path -> to.path: >>> " + from.path + " -> " + to.path);
+
+  let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+
+  if (flag) {
+    if (to.path.indexOf("mobile") === -1) {
+      to = {
+        path: "/mobile" + to.path
+      }
+      console.log(" new from.path -> to.path: >>> " + from.path + " -> " + to.path);
+      if (to.path !== from.path) {
+        router.replace(to.path).catch(e => ({}));
+      }
+    }
+  }
+
   NProgress.start()
   if (getToken()) {
-    if (to.path === '/login') {
-      next({ path: '/' })
+    if (to.path === '/login' || to.path === '/mobile/login') {
+      next({path: '/'})
       NProgress.done()
     } else {
       if (Object.keys(store.getters.info).length === 0) {
         store.dispatch('GetUserId').then(res => {
           const userId = res.id
-          store.dispatch('GetInfo', { userId }).then(accessRoutes => {
+          store.dispatch('GetInfo', {userId}).then(accessRoutes => {
             next()
           })
         }).catch(err => {
           store.dispatch('LogOut').then(() => {
             Message.error(err)
-            next({ path: '/' })
+            next({path: '/'})
           })
         })
       } else {
